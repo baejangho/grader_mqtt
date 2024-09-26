78c666b1965acd96364fe8b62603bf5fbbffdd5d
Jangho Bae <janghobae@ks.ac.kr>
2024-09-26 18:26:30 +0900

    ver1.0

---
+++ publish_test.py
@@ -0,0 +1,58 @@
+# paho.mqtt.client 모듈을 이용한 MQTT 메세지 송수신
+
+import paho.mqtt.client as mqtt
+import time
+
+sub_topic = "command/workplan/grader"
+pub_topic = "equipment/grader"
+withpoint_url = "withpoints.asuscomm.com"   # 브로커 주소
+
+payload ={
+  "grd_mac_addr": "AB:BC:CD:DE:EF:F0",
+  "grd_protocol_id": "A",
+  "grd_type": 1,
+  "grd_id": "ABCD-1243-QWER-5678",
+  "Timestamp": "2022-05-16 17:00:00",
+  "grd_bdy_lttd": 35.943114,
+  "grd_bdy_lgtd": 126.577257,
+  "grd_bdy_altd": 67.596,
+  "grd_bdy_x_pose": 50.0,
+  "grd_bdy_y_pose": 50.0,
+  "grd_bdy_z_pose": 50.0,
+  "grd_bdy_velocity": 10.0,
+  "grd_bld_x_pose": 0.0,
+  "grd_bld_y_pose": 0.0,
+  "grd_bld_z_pose": 0.0,
+  "grd_bld_z_offset": 0.0,
+  "grd_running_state": 1,
+  "grd_running_mode": 0,
+  "grd_reverse_drv": False,
+  "grd_rdr_detect":0
+} 
+
+# callback 함수 정의
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe(sub_topic) # 구독 신청
+def on_message(client, userdata, msg): # 구독한 메시지 수신
+    print(msg.topic+" "+str(msg.payload))
+def on_publish(client, userdata, mid): # 발행한 메시지 확인
+    print("Message Published...")
+
+mqtt_client = mqtt.Client(client_id="grader_01")    # 클라이언트 객체 생성
+mqtt_client.username_pw_set("grader", "grader") # 사용자 인증정보 설정
+mqtt_client.on_connect = on_connect # 연결 콜백 지정
+mqtt_client.on_message = on_message # 메시지 수신 콜백 지정
+mqtt_client.on_publish = on_publish # pub 수신 콜백 지정
+
+
+mqtt_client.connect(withpoint_url, 50592, 60) # 브로커에 연결
+
+mqtt_client.loop_start()
+
+while True:
+    mqtt_client.publish(pub_topic, str(payload), qos=0, retain=False) # 메시지 발행
+    time.sleep(1) # 1초 대기
+
+
+
